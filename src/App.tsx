d4cb1f2beaa4b5d29e0c0fe546ab9a4d907b4240
Hamres <h4mres@gmail.com>
2023-04-21 08:35:30 +0300

    lazy

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,45 @@
-import React from 'react';
+import React, {Suspense} from 'react';
 import {Route, Routes} from "react-router-dom";
+import Loadable from 'react-loadable';
 
 import Home from "./pages/Home";
-import Cart from "./pages/Cart";
-import NotFound from "./pages/NotFound";
-import FullPizza from "./pages/FullPizza";
-import MainLayout from "./layouts/MainLayout";
 
+import MainLayout from "./layouts/MainLayout";
 import './scss/app.scss'
+
+// const Cart = React.lazy(() => import(/* webpackChunkName: "Cart" */ './pages/Cart'));
+
+const Cart = Loadable({
+    loader: () => import(/* webpackChunkName: "Cart" */ './pages/Cart'),
+    loading: () => <div>Загрузка...</div>,
+});
+
+const NotFound = React.lazy(() => import(/* webpackChunkName: "NotFound" */ './pages/NotFound'));
+const FullPizza = React.lazy(() => import(/* webpackChunkName: "FullPizza" */ './pages/FullPizza'));
 
 const App = () => {
     return (
         <Routes>
             <Route path='/' element={<MainLayout />}>
                 <Route path='' element={<Home />} />
-                <Route path='cart' element={<Cart />} />
-                <Route path='pizza/:id' element={<FullPizza />} />
-                <Route path='*' element={<NotFound />} />
+                <Route path='cart' element={
+                    <Suspense fallback={<div>Загрузка...</div>}>
+                        <Cart/>
+                    </Suspense>
+                }
+                />
+                <Route path='pizza/:id' element={
+                    <Suspense fallback={<div>Загрузка...</div>}>
+                        <FullPizza />
+                    </Suspense>
+                }
+                />
+                <Route path='*' element={
+                    <Suspense fallback={<div>Загрузка...</div>}>
+                        <NotFound />
+                    </Suspense>
+                }
+                />
             </Route>
 
         </Routes>
